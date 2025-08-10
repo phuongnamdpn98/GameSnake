d2a17dae1a9815e82b799583fd220efd113c7a30
Dang Nam <116860706+phuongnamdpn98@users.noreply.github.com>
2025-08-10 20:43:50 +0700

    add Game function

--- Program.cs
+++ Program.cs
@@ -16,9 +16,98 @@
     {
         Up, Down, Left, Right
     }
+
+    public static void DrawBorder(int width, int height)
+    {
+        Console.Clear();
+        for (int x = 0; x < width; x++)
+        {
+            Console.SetCursorPosition(x, 0);
+            Console.Write("_");
+            Console.SetCursorPosition(x, height - 1);
+            Console.Write("-");
+        }
+        for (int y = 0; y < height; y++)
+        {
+            Console.SetCursorPosition(0, y);
+            Console.Write("|");
+            Console.SetCursorPosition(width - 1, y);
+            Console.Write("|");
+        }
+    }
+
+    public static void Game(int level)
+    {
+        int width = 40;
+        int height = 20;
+        Point food;
+        List<Point> snake = new List<Point>();
+        Direction direction = Direction.Right;
+        Random rand = new Random();
+        int score = 0;
+
+        food = new Point(rand.Next(1, width - 2), rand.Next(1, height - 2));
+
+        snake.Add(new Point(12, 10));
+        snake.Add(new Point(11, 10));
+        snake.Add(new Point(10, 10));
+
+        DateTime lastTime = DateTime.Now;
+        while (true)
+        {
+            DrawBorder(width, height);
+
+            foreach (Point s in snake)
+            {
+                Console.SetCursorPosition(s.X, s.Y);
+                Console.Write("O");
+            }
+
+            Console.SetCursorPosition(food.X, food.Y);
+            Console.Write("*");
+
+            if (Console.KeyAvailable)
+            {
+                ConsoleKey key = Console.ReadKey(true).Key;
+                switch (key)
+                {
+                    case ConsoleKey.W:
+                        if (direction != Direction.Down)
+                        {
+                            direction = Direction.Up;
+                        }
+                        break;
+                    case ConsoleKey.S:
+                        if (direction != Direction.Up)
+                        {
+                            direction = Direction.Down;
+                        }
+                        break;
+                    case ConsoleKey.A:
+                        if (direction != Direction.Right)
+                        {
+                            direction = Direction.Left;
+                        }
+                        break;
+                    case ConsoleKey.D:
+                        if (direction != Direction.Left)
+                        {
+
+                            direction = Direction.Right;
+                        }
+                        break;
+                }
+            }
+        }
+
+        Console.SetCursorPosition(0, height = 1);
+    }
+
     public static void Main(string[] args)
     {
         int choiceMenu = -1;
+        Console.CursorVisible = false;
+
         do
         {
             Console.WriteLine("Menu:");
@@ -55,8 +144,7 @@
                             Console.WriteLine("Invalid level selected. Please try again.");
                             break;
                     }
-
-
+                    Game(level);
                     break;
                 case 0:
                     Environment.Exit(0);
